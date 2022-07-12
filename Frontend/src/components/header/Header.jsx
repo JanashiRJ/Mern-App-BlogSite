3a1771ab62ecf15630544e81ed2f0a0a8c0846f4
JanashiRJ <96720@fhss.sjp.ac.lk>
2022-07-12 15:31:28 +0530

    Header.jsx file coding

--- Frontend/src/components/header/Header.jsx
+++ Frontend/src/components/header/Header.jsx
@@ -4,12 +4,12 @@ export default function Header() {
   return (
     <div className="header">
       <div className="headerTitles">
-        <span className="headerTitleSm">React & Node</span>
+        <span className="headerTitleSm">Seasonal Flowers</span>
         <span className="headerTitleLg">Blog</span>
       </div>
       <img
         className= "headerImg"
-        src="https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        src="https://images.unsplash.com/photo-1554773228-1f38662139db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MnwxMDAwODk5MXx8ZW58MHx8fHw%3D&w=1000&q=80"
         alt= ""
       />
     </div>
