5c3566e0cbcdb5a2d8066b52cc68b3ad75bd00a3
JanashiRJ <96720@fhss.sjp.ac.lk>
2022-07-17 12:16:39 +0530

    Change Backgroun image

--- Frontend/src/components/header/Header.jsx
+++ Frontend/src/components/header/Header.jsx
@@ -5,11 +5,11 @@ export default function Header() {
     <div className="header">
       <div className="headerTitles">
         <span className="headerTitleSm">Seasonal Flowers</span>
-        <span className="headerTitleLg">Blog</span>
+
       </div>
       <img
         className= "headerImg"
-        src="https://images.unsplash.com/photo-1554773228-1f38662139db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MnwxMDAwODk5MXx8ZW58MHx8fHw%3D&w=1000&q=80"
+        src="https://www.securitypology.com/wp-content/uploads/Sakura-tunnel-Miyagi.jpg"
         alt= ""
       />
     </div>
