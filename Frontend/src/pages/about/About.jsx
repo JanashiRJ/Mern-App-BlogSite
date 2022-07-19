6dc7196d60cadcce6987c3609fea2364d2cbb04f
JanashiRJ <96720@fhss.sjp.ac.lk>
2022-07-19 17:28:58 +0530

    Add details about spring flowers

--- Frontend/src/pages/about/About.jsx
+++ Frontend/src/pages/about/About.jsx
@@ -11,6 +11,29 @@ export default function About() {
         <span className="title">About Seasonal Flowers</span>
       </div>
       </div>
+      <div>
+      <span className="title1">FLOWERS AND SEASONS</span>
+      </div>
+      <div>
+      <span className="para1">Flowers & Seasons are intimately bound to each other.
+      Most of the flowers are season - specific. Though some flowers are found throughout the year, 
+      there are some not particularly affected by changing seasons, viz.,
+      Alstromeria, Anthurium, Aster, Bourvardia, Carnation, Chrysanthemum, Gerbera Daisy, 
+      Lily, Asiatic Lily.The various climatic changes that occur in cyclic patterns are termed as 'seasons'. 
+      There are four general seasons occurring on Earth - Spring, Summer, Autumn and Winter.</span>
+      </div>
+      <div>
+      <span className="title2">1 Spring Flowers</span>
+      </div>
+      <div>
+      <span className="para2">Summer is the time of hot and warm weather. 
+      Floral growth is best in the summer season. 
+      The Northern hemisphere experiences summer during June, July, and August, while in the Southern hemisphere, during December - February.
+      Summer is the season of the Midnight sun in the North pole and Ice land.
+      The Flowers Blooming in Summer are:
+      <span className="para3">Alchemilla, Allium, Alstromeria, Amaranthus, Baby's Breath , Bird of Paradise, Calla lily, Campanula, Carnation, Chrysanthemum, Cockscomb, Cosmos, Dahlia, Delphinium, Dianthus, Didiscus, Euphorbia, Foxglove, Freesia, Gardenia, Genista, Ginger, Gladiolus, Hallaconia, Heather, Hydrangea, Hypericum, Iris, Kangaroo paw, Liatrus, Lilac, Casa Blanca Lily, Gloriosa Lily, Star Gazer, Lisianthus.</span>
+      </span>
+      </div>
     </div>
   );
 }
