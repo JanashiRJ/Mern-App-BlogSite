9f08ad42335c1ac26d39815586d5462b0aeb51c0
JanashiRJ <96720@fhss.sjp.ac.lk>
2022-07-19 16:46:48 +0530

    Add Topic and image to about.jsx

--- Frontend/src/pages/about/About.jsx
+++ Frontend/src/pages/about/About.jsx
@@ -1,1 +1,17 @@
+import "./about.css";
+
+
+export default function About() {
+  return(
+    <div className="write">
+      <div>
+        <img src="https://media.istockphoto.com/photos/easter-flowers-on-open-christian-bible-picture-id508513752?k=20&m=508513752&s=612x612&w=0&h=ndrVTUq5ymIgl-9DfxMQ3JPzY_Ik9Z9819h8r37w_8A=" 
+        width="100%" height="400px"></img>
+        <div>
+        <span className="title">About Seasonal Flowers</span>
+      </div>
+      </div>
+    </div>
+  );
+}
 
