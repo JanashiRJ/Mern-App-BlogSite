9b78ac3f7949c721e031623929d33b6cdaafef36
JanashiRJ <96720@fhss.sjp.ac.lk>
2022-07-20 20:24:55 +0530

    Edit index.js file

--- Backend/index.js
+++ Backend/index.js
@@ -40,7 +40,13 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
-app.use("/api/categories", categoryRoute);
+app.use("/api/categories", categoryRoute); 
+
+app.use(express.static(path.join(__dirname, "/Frontend /build")));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/Frontend /build', 'index.html'));
+});
 
 app.listen(process.env.PORT || 5000, ()=>{
     console.log("Backend is running")
