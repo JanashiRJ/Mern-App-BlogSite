aea55eca630ab7120f637462b4bf76f3683c1fc0
JanashiRJ <96720@fhss.sjp.ac.lk>
2022-07-20 14:37:29 +0530

    Add a background image to contact page

--- Frontend/src/pages/contact/Contact.jsx
+++ Frontend/src/pages/contact/Contact.jsx
@@ -3,7 +3,13 @@ import "./contact.css";
 export default function contact() {
   return(
     <div className="contact">
-        <span>Hello</span>
+    <div className="background">
+    <div className="contactimage">
+    <img src="https://www.iconpacks.net/icons/3/free-icon-mail-5888.png"
+       className="imageuser" >
+       </img>
+    </div>
+    </div>
     </div>
   );
 }
