dbe3a6189847617237f6b4188a6cde4e6fa8f7cb
JanashiRJ <96720@fhss.sjp.ac.lk>
2022-07-20 14:39:08 +0530

    Add a form into contact page

--- Frontend/src/pages/contact/Contact.jsx
+++ Frontend/src/pages/contact/Contact.jsx
@@ -8,6 +8,33 @@ export default function contact() {
     <img src="https://www.iconpacks.net/icons/3/free-icon-mail-5888.png"
        className="imageuser" >
        </img>
+       <span className="loginTitle">Contact Us</span>
+      <form className="loginForm">
+        <label>Your Name
+        <input
+          type="text"
+          className="Input1"
+          placeholder="Enter your name..."
+        />
+        </label>
+        <label>User Email
+        <input
+          type="text"
+          className="Input2"
+          placeholder="Enter your email..."
+        />
+        </label>
+        <label>Message
+        <input
+          type="text"
+          className="Input3"
+          placeholder="Type your message..."
+        />
+        </label>
+        <button className="Button" type="submit">
+          Send
+        </button>
+      </form>
     </div>
     </div>
     </div>
