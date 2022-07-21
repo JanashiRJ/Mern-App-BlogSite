b1b3ed576f2256ba0e87703249339e056160757c
JanashiRJ <96720@fhss.sjp.ac.lk>
2022-07-21 11:38:50 +0530

    Add Frontend to backend and code changes

--- Backend/Frontend/src/config.js
+++ Backend/Frontend/src/config.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL : "https://janashiflowerblog.herokuapp.com/api/"
+    baseURL : "https://janashiflowersblog.herokuapp.com/api/"
 })
